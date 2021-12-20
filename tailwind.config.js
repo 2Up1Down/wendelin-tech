dda36087e7b1189ba4c9ebde64d63fb7304dd21d
Wendelin <wendelin.steiner@gmail.com>
2021-12-20 18:22:02 +0100

    deleted initial styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}', './slices/**/*.{html,js}'],
   theme: {
+    fontFamily: {
+      Poppins: ['Poppins, sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
     extend: {},
   },
   plugins: [],
