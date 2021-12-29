d10ac0c324c1502938c243d8ad86c2ec4824127e
Wendelin <wendelin.steiner@gmail.com>
2021-12-29 15:37:07 +0100

    changed testimonials to grid view

--- slices/SectionTestimonials/index.js
+++ slices/SectionTestimonials/index.js
@@ -3,7 +3,7 @@ import { RichText } from 'prismic-reactjs';
 import Image from 'next/image';
 
 const SectionTestimonials = ({ slice }) => (
-  <section>
+  <section className="bg-gray-100">
     <div>
       <h3 className="text-2xl">
         <RichText render={slice.primary.title} />
@@ -13,16 +13,10 @@ const SectionTestimonials = ({ slice }) => (
         <RichText render={slice.primary.description} />
       </div>
     </div>
-    <div className="flex flex-row">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {slice?.items?.map((item, i) => (
-        <div key={i} className="">
-          <span>{item.heading}</span>
-          <RichText render={item.citation} />
-          <span>{item.author}</span>
-          <span>{item.position}</span>
-
-          <img src={item.image.url} alt={item.image.alt} />
-          <div className="relative aspect-square w-full">
+        <div key={i} className="flex flex-col items-center">
+          <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
             <Image
               src={item.image.url}
               alt={item.image.alt}
@@ -31,6 +25,12 @@ const SectionTestimonials = ({ slice }) => (
               objectPosition="center"
             />
           </div>
+          <div className="h2 text-lg text-center">{item.heading}</div>
+          <div className="text-sm text-center">
+            <RichText render={item.citation} />
+          </div>
+          <div className="text-base text-gray-700">{item.author}</div>
+          <div className="text-xs relative text-gray-500">{item.position}</div>
         </div>
       ))}
     </div>
