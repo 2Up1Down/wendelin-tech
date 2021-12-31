f858b889ddb6d2ec65d5a758852d43bdbadf6b5d
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-12-31 15:31:58 +0100

    refactoring of testimonial section

--- components/testimonial.js
+++ components/testimonial.js
@@ -6,15 +6,17 @@ const Testimonial = ({ testimonial }) => {
   const { image, citation, author, position } = testimonial;
   return (
     <figure className="flex flex-col items-center">
-      <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
-        <Image
-          src={image.url}
-          alt={image.alt}
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-        />
-      </div>
+      {image.url && (
+        <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
+          <Image
+            src={image.url}
+            alt={image.alt}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+      )}
       <blockquote className="text-sm text-center">
         <RichText render={citation} />
       </blockquote>
--- slices/SectionTestimonials/index.js
+++ slices/SectionTestimonials/index.js
@@ -1,48 +1,26 @@
 import React from 'react';
 import { RichText } from 'prismic-reactjs';
-import Image from 'next/image';
+import Testimonial from '../../components/testimonial';
 
 const SectionTestimonials = ({ slice }) => {
   console.log(slice.items[0].image.url);
   return (
     <section className="bg-gray-100">
-      <div>
-        <h3 className="text-2xl">
-          <RichText render={slice.primary.title} />
-        </h3>
-
+      <div className="container flex flex-col items-center">
         <div>
-          <RichText render={slice.primary.description} />
+          <h3 className="text-2xl">
+            <RichText render={slice.primary.title} />
+          </h3>
+
+          <div>
+            <RichText render={slice.primary.description} />
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {slice?.items?.map((item, i) => {
-          console.log('Testimonial');
-          console.log(item);
-          console.log('image URL:');
-          console.log(item.image);
-          return (
-            <div key={i} className="flex flex-col items-center">
-              {item.image.url && (
-                <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
-                  <Image
-                    src={item.image.url}
-                    alt={item.image.alt}
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                  />
-                </div>
-              )}
-              <div className="h2 text-lg text-center">{item.heading}</div>
-              <div className="text-sm text-center">
-                <RichText render={item.citation} />
-              </div>
-              <div className="text-base text-gray-700">{item.author}</div>
-              <div className="text-xs relative text-gray-500">{item.position}</div>
-            </div>
-          );
-        })}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {slice?.items?.map((testimonial, i) => (
+            <Testimonial key={i} testimonial={testimonial} />
+          ))}
+        </div>
       </div>
     </section>
   );
