8eabf3e2e59177d276d757a9995a9ba4f8f4c8f8
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-12-31 15:13:32 +0100

    fixed bug on next images

--- slices/SectionTestimonials/index.js
+++ slices/SectionTestimonials/index.js
@@ -16,25 +16,33 @@ const SectionTestimonials = ({ slice }) => {
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {slice?.items?.map((item, i) => (
-          <div key={i} className="flex flex-col items-center">
-            <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
-              <Image
-                src="https://source.unsplash.com/random"
-                alt={item.image.alt}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-              />
+        {slice?.items?.map((item, i) => {
+          console.log('Testimonial');
+          console.log(item);
+          console.log('image URL:');
+          console.log(item.image);
+          return (
+            <div key={i} className="flex flex-col items-center">
+              {item.image.url && (
+                <div className="relative aspect-square w-1/2 rounded-full overflow-hidden mb-2">
+                  <Image
+                    src={item.image.url}
+                    alt={item.image.alt}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
+                </div>
+              )}
+              <div className="h2 text-lg text-center">{item.heading}</div>
+              <div className="text-sm text-center">
+                <RichText render={item.citation} />
+              </div>
+              <div className="text-base text-gray-700">{item.author}</div>
+              <div className="text-xs relative text-gray-500">{item.position}</div>
             </div>
-            <div className="h2 text-lg text-center">{item.heading}</div>
-            <div className="text-sm text-center">
-              <RichText render={item.citation} />
-            </div>
-            <div className="text-base text-gray-700">{item.author}</div>
-            <div className="text-xs relative text-gray-500">{item.position}</div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
