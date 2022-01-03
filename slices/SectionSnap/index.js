9cf5ee9ce596f509b1573a3bc1a66d0032c0b855
Wendelin <wendelin.steiner@gmail.com>
2022-01-03 10:58:06 +0100

    changes SectionSnap to be in a repeatable zone

--- slices/SectionSnap/index.js
+++ slices/SectionSnap/index.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { RichText, Link } from 'prismic-reactjs';
 import Image from 'next/image';
 
-const SectionSnap = ({ slice }) => (
-  <section className="bg-slate-100">
-    <div className="container flex bg-slate-300 p-2">
-      <div className="flex-1 flex flex-col justify-start">
-        <div className="">
-          <h1 className="text-center">
-            <RichText render={slice.primary.title} />
-          </h1>
-        </div>
-        <div>
-          <RichText render={slice.primary.description} />
-        </div>
-        <div className="bg-black text-white  border-none rounded">
-          <a href={Link.url(slice.primary.buttonLink)} className="text-white p-2">
-            {slice.primary.button}
-          </a>
-        </div>
+const SectionSnap = ({ slice }) => {
+  console.log(slice.items);
+  return (
+    <section className="bg-slate-100">
+      <div className="container flex bg-slate-300 p-2">
+        {slice?.items?.map((snap, i) => (
+          <div key={i}>
+            <div className="flex-1 flex flex-col justify-start">
+              <div className="">
+                <h1 className="text-center">
+                  <RichText render={snap.title} />
+                </h1>
+              </div>
+              <div>
+                <RichText render={snap.description} />
+              </div>
+              <div className="bg-black text-white  border-none rounded">
+                <a href={Link.url(snap.buttonLink)} className="text-white p-2">
+                  {snap.buttonText}
+                </a>
+              </div>
+            </div>
+            <div className="flex-1  ">
+              <img src={snap.image.url} alt={snap.image.alt} />
+            </div>
+          </div>
+        ))}
       </div>
-      <div className="flex-1  ">
-        <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default SectionSnap;
