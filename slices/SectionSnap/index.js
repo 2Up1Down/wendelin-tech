beb0f74eb62316ac2fa00ce34a38297ab1f77a65
Wendelin <wendelin.steiner@gmail.com>
2022-01-03 22:15:55 +0100

    added snap effect to section-snap

--- slices/SectionSnap/index.js
+++ slices/SectionSnap/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { RichText, Link } from 'prismic-reactjs';
+import { RichText } from 'prismic-reactjs';
+import Link from 'next/link';
 import Image from 'next/image';
 
 const SectionSnap = ({ slice }) => {
-  console.log(slice.items);
+  // console.log(slice.items);
   return (
-    <section className="bg-slate-100">
-      <div className="container flex bg-slate-300 p-2">
+    <section className="max-h-screen overflow-y-scroll snap-y snap-mandatory">
+      <div className="">
         {slice?.items?.map((snap, i) => (
-          <div key={i}>
-            <div className="flex-1 flex flex-col justify-start">
-              <div className="">
-                <h1 className="text-center">
-                  <RichText render={snap.title} />
-                </h1>
+          <article key={i} className="h-screen snap-center odd:bg-gray-100 even:bg-white">
+            <div className="container flex place-items-center h-full">
+              <div className="flex-1 flex flex-col justify-start">
+                {snap.title && <h2 className="">{snap.title}</h2>}
+                {snap.subtitle && <h4 className="">{snap.subtitle}</h4>}
+                {snap.description && <RichText render={snap.description} />}
+                {snap.buttonLink && (
+                  // <Link href={snap.buttonLink}>
+                  //   <a className="">{snap.buttonText}</a>
+                  // </Link>
+                  <a>--here comes the button link--</a>
+                )}
               </div>
-              <div>
-                <RichText render={snap.description} />
-              </div>
-              <div className="bg-black text-white  border-none rounded">
-                <a href={Link.url(snap.buttonLink)} className="text-white p-2">
-                  {snap.buttonText}
-                </a>
+              <div className="flex-1">
+                <img src={snap.image.url} alt={snap.image.alt} />
               </div>
             </div>
-            <div className="flex-1  ">
-              <img src={snap.image.url} alt={snap.image.alt} />
-            </div>
-          </div>
+          </article>
         ))}
       </div>
     </section>
