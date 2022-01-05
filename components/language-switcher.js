afa36371048c597a56f92b73572267df01ffa144
Wendelin <wendelin.steiner@gmail.com>
2022-01-05 14:03:27 +0100

    refactored language switch

--- components/language-switcher.js
+++ components/language-switcher.js
@@ -2,18 +2,18 @@ import NextLink from 'next/link';
 import React from 'react';
 import { linkResolver } from '../prismicConfiguration';
 
-const AltLangs = ({ altLangs = [] }) =>
-  altLangs.map((altLang) => {
-    return (
-      <li className="" key={altLang.id}>
-        <NextLink locale={altLang.lang} href={linkResolver(altLang)}>
-          <a className="">{altLang.lang}</a>
-        </NextLink>
-        <style jsx>{``}</style>
-      </li>
-    );
-  });
-
-const LanguageSwitcher = ({ altLangs }) => <AltLangs altLangs={altLangs} />;
+const LanguageSwitcher = ({ altLangs = [] }) => {
+  return (
+    <ul>
+      {altLangs.map((altLang) => (
+        <li className="" key={altLang.id}>
+          <NextLink locale={altLang.lang} href={linkResolver(altLang)}>
+            <a className="">{altLang.lang}</a>
+          </NextLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default LanguageSwitcher;
