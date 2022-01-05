7e5114f00495e5a79ef653004b4170df0c8f6acb
Wendelin <wendelin.steiner@gmail.com>
2022-01-05 14:31:32 +0100

    added conditional rendering on footer

--- components/footer.js
+++ components/footer.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import Image from 'next/image';
 import { RichText } from 'prismic-reactjs';
+
 import SocialIcon from './social-icon';
+import { CustomLink } from '../utils/prismicHelpers';
 
 const Footer = ({ logo, menu, info, social }) => {
   return (
     <footer>
       <div className="container flex flex-col place-items-center">
-        <div className="h-16 w-64 relative">
-          <Image
-            src={logo.url}
-            alt={logo.alt}
-            layout="fill"
-            objectFit="contain"
-            className="object-center"
-          />
-        </div>
-        <RichText render={info} />
-        <div className="flex flex-row gap-2">
-          {social &&
-            Object.keys(social).map((item) => (
+        {logo.url && (
+          <div className="h-16 w-64 relative">
+            <Image
+              src={logo.url}
+              alt={logo.alt}
+              layout="fill"
+              objectFit="contain"
+              className="object-center"
+            />
+          </div>
+        )}
+        {info && <RichText render={info} />}
+        {info && <RichText render={info} serializeHyperlink={CustomLink} />}
+
+        {social && (
+          <div className="flex flex-row gap-2">
+            {Object.keys(social).map((item) => (
               <SocialIcon key={item} channel={item} url={social[item]} />
             ))}
-        </div>
+          </div>
+        )}
+
         <p>
           <small>&#169; {new Date().getFullYear()} Wendelin Steiner Technologies</small>
         </p>
--- utils/prismicHelpers.js
+++ utils/prismicHelpers.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { apiEndpoint, accessToken, linkResolver, Router } from '../prismicConfiguration';
 
 // Helper function to convert Prismic Rich Text links to Next/Link components
-export const customLink = (type, element, content, children, index) => (
+export const CustomLink = (type, element, content, children, index) => (
   <Link key={index} href={linkResolver(element.data)}>
     <a>{content}</a>
   </Link>
